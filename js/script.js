be371c3a495507a558cdb20be6f6ba65ced124db
Lucian Condurache <condurachelucian@yahoo.com>
2019-03-03 20:58:54 +0000

    Added project files

---
+++ js/script.js
@@ -0,0 +1,82 @@
+let quotes = [
+  {
+    quote: "Happiness can exist only in acceptance.",
+    author: "George Orwell",    
+  },
+  {
+    quote: "Love has no age, no limit; and no death.",
+    author: "John Galsworthy"
+  },
+  {
+    quote: "You can't blame gravity for falling in love.",
+    author: "Albert Einstein"
+  },
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: "Thomas A. Edison"
+  },
+  {
+    quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    author: "Benjamin Franklin"
+  },
+  {
+    quote: "The secret of getting ahead is getting started.",
+    author: "Mark Twain"
+  },
+  {
+    quote: "We love life, not because we are used to living but because we are used to loving.",    
+    author: "Friedrich Nietzsche"
+  },
+  {
+    quote: "If you cannot do great things, do small things in a great way.",
+    author: "Napoleon Hill"
+  },
+  {
+    quote: "The supreme art of war is to subdue the enemy without fighting.",
+    author: "Sun Tzu"
+  },
+  {
+    quote: "You can't blame gravity for falling in love.",
+    author: "Albert Einstein"
+  },
+  {
+    quote: "Honesty is the first chapter in the book of wisdom.",
+    author: "Thomas Jefferson"
+  },
+  {
+    quote: "Very little is needed to make a happy life; it is all within yourself, in your way of thinking.",
+    author: "Marcus Aurelius"
+  },
+  {
+    quote: "Life isn't about finding yourself. Life is about creating yourself.",
+    author: "George Bernard Shaw"
+  },
+  {
+    quote: "The only true wisdom is in knowing you know nothing.",
+    author: "Socrates"
+  },
+  {
+    quote: "A kiss is a lovely trick designed by nature to stop speech when words become superfluous.",
+    author: "Ingrid Bergman"
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+  },
+  {
+    quote: "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost"
+  },
+  {
+    quote: "Blessed are the hearts that can bend; they shall never be broken.",
+    author: "Albert Camus"
+  },
+  {
+    quote: "Where there is love there is life.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+    author: "Robert Louis Stevenson"
+  }
+];
